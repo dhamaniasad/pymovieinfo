4751cb884e466c1e0b6a11f10165ce8ef4ff8750
Asad Dhamani <dhamaniasad@gmail.com>
2015-06-23 22:08:45 +0530

    Add command line functionality

--- pymovieinfo.py
+++ pymovieinfo.py
@@ -1,16 +1,36 @@
 import requests
 import json
+import argparse
 
 query = ""
 
-url = "http://www.omdbapi.com/?t={}&type=movie".format(query)
+parser = argparse.ArgumentParser(description='Get information about any movie from the command line')
+parser.add_argument('movie', type=str, help="Name of the movie you want information about")
+args = parser.parse_args()
+movie = args.movie
 
-r = requests.get(url)
-data = json.loads(r.content)
-movie_runtime = int(data['Runtime'].split()[0])
-print "Movie: {}".format(data['Title'])
-print "Genre: {}".format(data['Genre'])
-print "Released: {}".format(data['Year'])
-print "Runtime: {} hours {} minutes".format(movie_runtime / 60, movie_runtime % 60)
-print "Plot: {}".format(data['Plot'])
-print "IMDb rating: {} stars from {} votes".format(data['imdbRating'], data['imdbVotes'])
+def get_info(movie):
+    url = "http://www.omdbapi.com/?t={}&type=movie".format(movie)
+    r = requests.get(url)
+    data = json.loads(r.content)
+    if data['Response'] == "True":
+        movie_runtime = int(data['Runtime'].split()[0])
+        rating = "{} stars from {} votes".format(data['imdbRating'], data['imdbVotes'])
+        movie_runtime = "{} hours {} minutes".format(movie_runtime / 60, movie_runtime % 60)
+        movie_info = {'title': data['Title'], 'genre': data['Genre'],
+                      'released': data['Year'], 'runtime': movie_runtime,
+                      'plot': data['Plot'], 'rating': rating}
+        return movie_info
+    else:
+        return False
+
+movie_data = get_info(movie)
+if movie_data is False:
+    print "Sorry, could not find a movie with that name :'("
+else:
+    print "Movie: {}".format(movie_data['title'])
+    print "Plot: {}".format(movie_data['plot'])
+    print "Genre: {}".format(movie_data['genre'])
+    print "Released: {}".format(movie_data['released'])
+    print "Runtime: {}".format(movie_data['runtime'])
+    print "IMDb rating: {}".format(movie_data['rating'])
